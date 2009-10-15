e574a1ec3fa6f30e7f1c6a4cb1ff5ab24b714e70
dmprice@users.sourceforge.net <dpe@dpe2010.localdomain>
2009-10-15 17:36:12 +0100

    Added support for Select as SelectItem

--- mapping_uml2.rb
+++ mapping_uml2.rb
@@ -6,12 +6,16 @@ require 'erb'
 # and performs a structural EXPRESS-to-UML2 (2.1.2) mapping using Ruby ERB templates.
 # The output is in XMI 2.1 syntax.
 # 
+# Integer, Boolean, String -> UML equivalent builtin type
+# Real, Number, Binary, Logical -> New PrimitiveType
 # Schema -> Package
 # Entity (subtype of) -> Class (Generalization)
 # Select Type -> Interface and InterfaceRealization
 # Enum Type -> Enumeration and EnumerationLiteral
 # Explicit Attribute (Optional) Primitive or Enum -> Property owned by Class (with lower)
-# 
+#
+#######################################################################################
+
 def map_from_express( mapinput )
 
 # datatypes for simple and aggregates of simple type
@@ -24,7 +28,7 @@ datatype_hash["BOOLEAN"] = 'http://schema.omg.org/spec/UML/2.1.2/uml.xml#Boolean
 # datatype_hash["LOGICAL"] = 'http://www.w3.org/2001/XMLSchema#boolean'
 datatype_hash["STRING"] = 'http://schema.omg.org/spec/UML/2.1.2/uml.xml#String'
 
-# Template covering the start of the output file 
+# XMI File Start Template
 overall_start_template = %{<?xml version="1.0" encoding="UTF-8"?>
 <uml:Model xmi:version = "2.1" xmlns:xmi = "http://schema.omg.org/spec/XMI/2.1" xmlns:uml = "http://schema.omg.org/spec/UML/2.1.2" name = "UMLfromEXPRESS" xmi:id = "_0">
 <packagedElement xmi:type="uml:PrimitiveType" xmi:id="REAL" name="Real" />
@@ -32,44 +36,46 @@ overall_start_template = %{<?xml version="1.0" encoding="UTF-8"?>
 <packagedElement xmi:type="uml:PrimitiveType" xmi:id="BINARY" name="Binary" />
 <packagedElement xmi:type="uml:PrimitiveType" xmi:id="LOGICAL" name="Logical" />}
 
-# Template covering the end of the output file 
+# XMI File End Template
 overall_end_template = %{  </uml:Model>}
 
-# Template covering the output file contents for each schema start
+# SCHEMA Start Template
 schema_start_template = %{<packagedElement xmi:type = "uml:Package" xmi:id = "_1_<%= schema.name %>" name = "<%= schema.name %>" visibility = "public">}
 
-# Template covering the output file contents for each schema end
+# SCHEMA End Template
 schema_end_template = %{</packagedElement>}
 
-# Template covering the output file contents for each entity type start
+# ENTITY Start Template
 entity_start_template = %{<packagedElement xmi:type = "uml:Class" xmi:id = "<%= xmiid %>" name = "<%= entity.name %>" isAbstract = "FALSE" visibility = "public">}
 
-# Template covering the output file contents for each entity type end
+# SUBTYPE OF Template
+supertype_template = %{<generalization xmi:type="uml:Generalization" xmi:id="<%= xmiid %>" general="<%= xmiid_supertype %>"/>}
+
+# ENTITY End Template
 entity_end_template = %{</packagedElement>}
 
-# Template covering the output file contents for each enum type start
+# ENUMERATION Start Template
 enum_start_template = %{<packagedElement xmi:type = "uml:Enumeration" xmi:id = "<%= enum_xmiid %>" name = "<%= enum.name %>">}
 
-# Template covering the output file contents for each enum type start
+# ENUMERATION ITEM Template
 enum_item_template = %{<ownedLiteral xmi:type="uml:EnumerationLiteral" xmi:id="<%= enumitem_xmiid %>" name="<%= enumitem %>" classifier="<%= enum_xmiid %>" enumeration="<%= enumitem_xmiid %>">
 <specification xmi:type="uml:LiteralInteger" xmi:id="<%= enumitem_xmiid + '_specification' %> "/>
 </ownedLiteral>}
 
-# Template covering the output file contents for each entity type end
+# ENUMERATION End Template
 enum_end_template = %{</packagedElement>}
 
-# Template covering the output file contents for each select type start
+# SELECT Start Template
 select_start_template = %{<packagedElement xmi:type = "uml:Interface" xmi:id = "<%= xmiid %>" name = "<%= select.name %>" isAbstract = "TRUE" visibility = "public">}
 
-# Template covering an entity type as a select type select item
-selectitem_template = %{ <interfaceRealization xmi:type="uml:InterfaceRealization" xmi:id="<%= xmiid %>" supplier="<%= xmiid_supplier %>" client="<%= xmiid_client %>" contract="<%= xmiid_supplier %>" implementingClassifier="<%= xmiid_client %>"/>}
+# SELECT ITEM IS ENTITY Template
+selectitem_entity_template = %{ <interfaceRealization xmi:type="uml:InterfaceRealization" xmi:id="<%= xmiid %>" supplier="<%= xmiid_supplier %>" client="<%= xmiid_client %>" contract="<%= xmiid_supplier %>" implementingClassifier="<%= xmiid_client %>"/>}
 
-# Template covering the output file contents for each select type end
+# SELECT ITEM IS SELECT Template
+selectitem_select_template = %{ <interfaceRealization xmi:type="uml:InterfaceRealization" xmi:id="<%= xmiid %>" supplier="<%= xmiid_supplier %>" client="<%= xmiid_client %>" contract="<%= xmiid_supplier %>" implementingClassifier="<%= xmiid_client %>"/>}
+
+# SELECT End Template
 select_end_template = %{</packagedElement>}
-
-# Template covering the supertype(s) for each entity type
-supertype_template = %{<generalization xmi:type="uml:Generalization" xmi:id="<%= xmiid %>" general="<%= xmiid_supertype %>"/>}
-
 
 # Template covering abstract entity types
 abstract_entity_template = %{}
@@ -89,7 +95,7 @@ attribute_entity_template = %{}
 # Template covering the output file contents for each attribute
 attribute_template = %{}
 
-# Template covering the output file contents for each attribute that is builtin datatype
+# EXPLICIT ATTRIBUTE SIMPLE TYPE Template
 attribute_builtin_template = %{
 <% if datatype_hash[attr.domain] != nil %>
 <ownedAttribute xmi:type="uml:Property" xmi:id="_<%= schema.name %>-<%= entity.name %>-<%= attr.name %>" name="<%= attr.name %>" visibility="public">
@@ -102,10 +108,11 @@ attribute_builtin_template = %{
    <lowerValue xmi:type="uml:LiteralInteger" xmi:id="_<%= schema.name %>-<%= entity.name %>-<%= attr.name %>_lowerValue"/>
  <% end %>	
 </ownedAttribute>}
+
 # Template covering the output file contents for each attribute that is builtin datatype
 attribute_entity_template = %{}
 
-# Template covering the output file contents for each attribute that is enum datatype
+# EXPLICIT ATTRIBUTE ENUM TYPE Template
 attribute_enum_template = %{<ownedAttribute xmi:type="uml:Property" xmi:id="_<%= schema.name %>-<%= entity.name %>-<%= attr.name %>" name="<%= attr.name %>" visibility="public" type="<%= enum_xmiid %>">
  <% if attr.isOptional == TRUE %>	
    <lowerValue xmi:type="uml:LiteralInteger" xmi:id="_<%= schema.name %>-<%= entity.name %>-<%= attr.name %>_lowerValue"/>
@@ -113,11 +120,10 @@ attribute_enum_template = %{<ownedAttribute xmi:type="uml:Property" xmi:id="_<%=
 </ownedAttribute>}
 
 
-
-
-
-
+#############################################################################################
 # Set up list of schemas to process, input may be a repository containing schemas or a single schema
+#############################################################################################
+
 if mapinput.kind_of? EXPSM::Repository
 	schema_list = mapinput.schemas
 elsif mapinput.kind_of? EXPSM::SchemaDefinition
@@ -169,7 +175,7 @@ for schema in schema_list
 	end
 
 # Handle select maps to UML Interface 
-	select_list = schema.contents.find_all{ |e| e.kind_of? EXPSM::TypeSelect }
+	select_list = schema.contents.find_all{ |e| e.instance_of? EXPSM::TypeSelect }
 	for select in select_list
 
 # Evaluate and write select start template 
@@ -177,6 +183,16 @@ for schema in schema_list
 		res = ERB.new(select_start_template)
 		t = res.result(binding)
 		file.puts t
+
+		for superselect in select_list
+			if superselect.selectitems_array.include?(select)
+				xmiid = '_2_superselect_' + schema.name + '-' + select.name + select_list.index(select).to_s + '-' + superselect.name
+				xmiid_supertype = '_1_select_' + schema.name + '-' + superselect.name + select_list.index(superselect).to_s
+				res = ERB.new(supertype_template)
+				t = res.result(binding)
+				file.puts t
+			end
+		end
 
 # Evaluate and write select end template 
 		res = ERB.new(select_end_template)
@@ -206,7 +222,7 @@ for schema in schema_list
 				xmiid = '_2_selectitem_' + schema.name + '-' + entity.name + entity_list.index(entity).to_s + '-' + select.name
 				xmiid_supplier = '_1_select_' + schema.name + '-' + select.name + select_list.index(select).to_s
 				xmiid_client = '_1_entity_' + schema.name + '-' + entity.name + entity_list.index(entity).to_s
-				res = ERB.new(selectitem_template)
+				res = ERB.new(selectitem_entity_template)
 				t = res.result(binding)
 				file.puts t
 			end

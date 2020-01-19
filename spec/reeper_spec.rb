d46d209f2348a6feb88d8e59ca659c819597147c
Abu Nashir <abunashir@gmail.com>
2020-01-19 16:10:55 +0100

    Add action setup to run test suite
    This commit adds the necessary setup to run the test suite for
    this gem. Currently, we are testing for ruby 2.6 and 2.5, but
    as soon as it is ready for production then we will enable more
    platform and specific versions.

--- spec/reeper_spec.rb
+++ spec/reeper_spec.rb
@@ -2,8 +2,4 @@ RSpec.describe Reeper do
   it "has a version number" do
     expect(Reeper::VERSION).not_to be nil
   end
-
-  it "does something useful" do
-    expect(false).to eq(true)
-  end
 end

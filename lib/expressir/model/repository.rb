5a7d9615cf187d04549ef4bb4c6152f76b48235b
Jan Zak <zj@zakjan.cz>
2021-07-21 23:47:46 +0200

    Update docs

--- lib/expressir/model/repository.rb
+++ lib/expressir/model/repository.rb
@@ -1,13 +1,11 @@
 module Expressir
   module Model
-      # Specified in ISO 10303-11:2004
-      # - section 10 Scope and visibility
-      # - section 10.3.13 Schema
+    # Multi-schema global scope
     class Repository < ModelElement
-      model_attr_accessor :schemas, 'Array<Schema>'
+      model_attr_accessor :schemas, 'Array<Declarations::Schema>'
 
       # @param [Hash] options
-      # @option options [Array<Schema>] :schemas
+      # @option options [Array<Declarations::Schema>] :schemas
       def initialize(options = {})
         @schemas = options[:schemas] || []
 

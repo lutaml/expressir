fe79ccd5063f8dbf55def2689e0eca15c9a844ca
Maxim [maxirmx] Samsonov <m.samsonov@computer.org>
2022-05-07 11:58:57 +0300

    Rubocop 3

--- spec/acceptance/version_spec.rb
+++ spec/acceptance/version_spec.rb
@@ -2,10 +2,13 @@ require "spec_helper"
 
 RSpec.describe "Expressir" do
   describe "version" do
+    it "has a version number" do
+      expect(Expressir::VERSION).not_to be nil
+    end  
+
     it "displays the current verison" do
       command = %w(version)
       output = capture_stdout { Expressir::Cli.start(command) }
-
       expect(output).to include("Version #{Expressir::VERSION}")
     end
   end

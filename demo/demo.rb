c47d346be4e9a2959a30f1e0cf082ee7ae2d9b0f
Maxim [maxirmx] Samsonov <m.samsonov@computer.org>
2022-05-07 17:44:11 +0300

    Add verification of packaged gems to CI script

--- demo/demo.rb
+++ demo/demo.rb
@@ -10,7 +10,7 @@ file = "action_schema.exp"
 # repo = Expressir::Express::Parser.from_exp(file)
 # schema = repo.schemas.find{|schema| schema.id == "support_resource_schema"}
 
-repo = Expressir::Express::Parser.from_exp(file)
+repo = Expressir::Express::Parser.from_file(file)
 schema = repo.schemas.find { |sch| sch.id == "action_schema" }
 entity = schema.entities.find { |ent| ent.id == "action_directive_relationship" }
 where_rule = entity.where_rules.find { |w_rule| w_rule.id == "WR1" }

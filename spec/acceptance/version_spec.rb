0857f9fbb8b0ff3ff111dda2f0b8ef10b81c68e4
maxirmx <m.samsonov@computer.org>
2024-01-26 16:55:54 +0300

    Added manually dispatchable stressed workflow to help address future issues

--- spec/acceptance/version_spec.rb
+++ spec/acceptance/version_spec.rb
@@ -4,6 +4,8 @@ RSpec.describe "Expressir" do
   describe "version" do
     it "has a version number" do |example|
       GC.stress = ENV["GC_STRESS"] == "true"
+      puts "Running tests in GC stress mode. It may take a couple of hours ..." if GC.stress
+
       print "\n[#{example.description}] "
       expect(Expressir::VERSION).not_to be nil
 

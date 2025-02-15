b748fb9b206ad90c408c368ea5078651c5f90437
Ronald Tse <ronald.tse@ribose.com>
2025-02-15 19:06:44 +0800

    feat: use lutaml-model liquid drops, delete old drops, set TODOs

--- lib/expressir/liquid.rb
+++ lib/expressir/liquid.rb
@@ -1,18 +1,1 @@
 require "liquid"
-
-require_relative "liquid/model_element_drop"
-
-require_relative "liquid/cache_drop"
-require_relative "liquid/data_type_drop"
-require_relative "liquid/declaration_drop"
-require_relative "liquid/expression_drop"
-require_relative "liquid/literal_drop"
-require_relative "liquid/reference_drop"
-require_relative "liquid/statement_drop"
-require_relative "liquid/supertype_expression_drop"
-require_relative "liquid/identifier_drop"
-require_relative "liquid/repository_drop"
-
-Dir[File.join(__dir__, "liquid", "**", "*.rb")].sort.each do |f|
-  require f
-end
--- lib/expressir/liquid/declaration_drop.rb
+++
@@ -1,8 +0,0 @@
-# frozen_string_literal: true
-
-module Expressir
-  module Liquid
-    class DeclarationDrop < ::Expressir::Liquid::ModelElementDrop
-    end
-  end
-end

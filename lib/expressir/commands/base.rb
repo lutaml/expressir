f509fdf150016792fd693e784e47750a6d4d549b
Ronald Tse <ronald.tse@ribose.com>
2025-03-28 12:14:24 +0800

    feat: refine coverage command with progress bar

--- lib/expressir/commands/base.rb
+++ lib/expressir/commands/base.rb
@@ -9,12 +9,23 @@ module Expressir
 
       # Common utilities that all commands might need
       def say(message)
-        puts message
+        # Use instance variable for testability - if output is set, use it
+        if defined?(@output) && @output
+          @output.puts(message)
+        else
+          puts(message)
+        end
       end
 
       def exit_with_error(message, exit_code = 1)
         say message
-        exit exit_code
+        # In test mode, raise an exception instead of exiting
+        # This makes it easier to test error cases
+        if defined?(@test_mode) && @test_mode
+          raise message # Just raise the message as an exception
+        else
+          exit exit_code
+        end
       end
     end
   end
